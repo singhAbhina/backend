7b548689ca8f2be902c593e69acd2f63a71efb9a
Abhinav Singh <abhi17may2004@gmail.com>
2025-07-30 21:49:59 +0530

    redis changes

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,12 +1,29 @@
-const { createClient }  = require('redis');
+// const { createClient }  = require('redis');
 
-const redisClient = createClient({
+// const redisClient = createClient({
+//     username: 'default',
+//     password: process.env.REDIS_PASS,
+//     socket: {
+//         host: 'redis-19027.c83.us-east-1-2.ec2.redns.redis-cloud.com',
+//         port: 19027
+//     }
+// });
+
+
+
+
+
+
+import { createClient } from 'redis';
+
+const client = createClient({
     username: 'default',
-    password: process.env.REDIS_PASS,
+    password: 'cus5vphUzd1RD13QjBUpd8NkhjmHPZqp',
     socket: {
         host: 'redis-19027.c83.us-east-1-2.ec2.redns.redis-cloud.com',
         port: 19027
     }
 });
-
 module.exports = redisClient;
+
+
