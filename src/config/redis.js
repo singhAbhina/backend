14a397b9e26a64792c69a1155c4550960537ee99
Abhinav Singh <abhi17may2004@gmail.com>
2025-07-30 22:05:04 +0530

    redis

--- src/config/redis.js
+++ src/config/redis.js
@@ -14,16 +14,31 @@
 
 
 
+// const { createClient } = require('redis');
+
+// const client = createClient({
+//     username: 'default',
+//     password: 'cus5vphUzd1RD13QjBUpd8NkhjmHPZqp',
+//     socket: {
+//         host: 'redis-19027.c83.us-east-1-2.ec2.redns.redis-cloud.com',
+//         port: 19027
+//     }
+// });
+// module.exports = redisClient;
+
+
+
+
 const { createClient } = require('redis');
 
-const client = createClient({
+const redisClient = createClient({
     username: 'default',
-    password: 'cus5vphUzd1RD13QjBUpd8NkhjmHPZqp',
+    password: process.env.REDIS_PASS,
     socket: {
         host: 'redis-19027.c83.us-east-1-2.ec2.redns.redis-cloud.com',
-        port: 19027
+        port: 19027,
+        tls: true
     }
 });
+
 module.exports = redisClient;
-
-
